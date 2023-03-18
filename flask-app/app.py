e43f739ca6976515908d3b05188d95ed21421022
Sem Haile (BN) <sem.haile@bonniernews.se>
2023-03-19 00:25:06 +0100

    aa

--- flask-app/app.py
+++ flask-app/app.py
@@ -28,15 +28,29 @@ def get_news(q=None, from_date=None):
     else:
         return {'error': 'Error fetching news'}
 
-@app.route('/form/', methods = ['POST', 'GET'])
+
+@app.route('/form/', methods=['POST', 'GET'])
 def form():
-    data = get_news()
-    print(data)
-    return render_template('form.html',data = data)
+    if request.method == 'POST':
+        q = request.form.get('q')
+        from_date = request.form.get('from_date')
+        data = get_news(q=q, from_date=from_date)
+        return render_template('form.html', data=data, q=q, from_date=from_date)
+    else:
+        data = get_news()
+        return render_template('form.html', data=data)
+
+
+# @app.route('/form/', methods = ['POST'])
+# def form_input():
+#     variable = request.form['q', 'from_date']
+
+#     return variable
+
 
 @app.route('/index/', methods = ['POST', 'GET'])
 def index():
-    data = get_news(q="tesla", from_date="2023-02-10")
+    data = get_news()
     print(data)
     
     return render_template('index.html', data = data)
@@ -53,8 +67,8 @@ def data():
  
 
 
-# if __name__ == '__main__':
-app.run(debug=True)
+if __name__ == '__app__':
+    app.run(debug=True)
 # @app.route('/api/news/blabla', methods=['GET'])
 # def news():
 #     q = request.args.get('q')
